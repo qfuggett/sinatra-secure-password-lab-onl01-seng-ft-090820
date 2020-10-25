48135cd70479b6b981d4a40a1d1306d59f1d1ffd
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-25 02:57:31 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20201025025029) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
